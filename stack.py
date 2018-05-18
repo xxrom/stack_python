51351b1958212c48e61137e6ce6f2fc125d7952e
Chernyshov <xpom55@mail.ru>
2018-05-18 14:53:36 +0300

    Stack / isEmpty, push, pop, peek, sizeStack/ LIFO last in first out

--- stack.py
+++ stack.py
@@ -1,3 +1,35 @@
 # LIFO last in first out
 
-class Stack
+class Stack:
+
+  def __init__(self):
+    self.stack = []
+
+  def isEmpty(self):
+    return self.stack == []
+
+  def push(self, data):
+    self.stack.append(data)
+
+  def pop(self):
+    data = self.stack[-1] # return last item
+    del self.stack[-1]
+    return data
+
+  def peek(self):
+    return self.stack[-1]
+
+  def sizeStack(self):
+    return len(self.stack)
+
+stack = Stack()
+
+stack.push(12)
+stack.push(32)
+stack.push(42)
+print(stack.sizeStack()) # [12 , 32, 42]
+print('pop ', stack.pop()) # 42
+print('pop ', stack.pop()) # 32
+print(stack.sizeStack()) # [12]
+print('peek ', stack.peek())
+print(stack.sizeStack()) # [12]
